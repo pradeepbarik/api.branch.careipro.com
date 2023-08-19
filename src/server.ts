769d2c478e1c0a44eb1a8274de3d34d018227c50
pradeep <pradeepbarik87@gmail.com>
2023-08-19 20:49:27 +0530

    ssl file configuration

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,68 @@
-//import https,{Server} from 'https';
-//import fs from 'fs';
-//import {SSL_KEY_FILE,SSL_CERT_FILE} from './config';
-import {IDbmethods,getDevelopmentDb,getProductionDb,getDevelopmentMongoDb,getProductionMongoDB} from './db';
+import https, { Server } from 'https';
+import fs from 'fs';
+import { SSL_KEY_FILE, SSL_CERT_FILE } from './config';
+import { IDbmethods, getDevelopmentDb, getProductionDb, getDevelopmentMongoDb, getProductionMongoDB } from './db';
 import APP from './app';
-//const key = fs.readFileSync(SSL_KEY_FILE);
-//const cert = fs.readFileSync(SSL_CERT_FILE);
-
-const PORT:number = <number><unknown>process.env.PORT;
-const DEV_MODE:string = <string>process.env.NODE_ENV;
-declare global{
-    var DB:IDbmethods;
-    var slaveDB:IDbmethods;
-    var MONGODB:any;
+const PORT: number = <number><unknown>process.env.PORT;
+const DEV_MODE: string = <string>process.env.NODE_ENV;
+declare global {
+    var DB: IDbmethods;
+    var slaveDB: IDbmethods;
+    var MONGODB: any;
 }
-console.log('dev_mode',DEV_MODE);
-switch(DEV_MODE){
+console.log('dev_mode', DEV_MODE);
+switch (DEV_MODE) {
     case 'development':
-        global.DB=getDevelopmentDb();
-        global.MONGODB=getDevelopmentMongoDb();
+        global.DB = getDevelopmentDb();
+        global.MONGODB = getDevelopmentMongoDb();
         break;
     case 'staging':
         break;
     case 'production':
-        global.DB=getProductionDb();
-        global.MONGODB=getProductionMongoDB();
+        global.DB = getProductionDb();
+        global.MONGODB = getProductionMongoDB();
         break;
     default:
-        global.DB=getDevelopmentDb();
+        global.DB = getDevelopmentDb();
 }
-//const SERVER:Server = https.createServer({ key, cert }, APP);
-const SERVER = APP.listen(PORT, () => {
-    console.log(`server is running ${PORT} dev_mode ${DEV_MODE}`);
-})
+if (DEV_MODE === 'development') {
+    const SERVER = APP.listen(PORT, () => {
+        console.log(`server is running ${PORT} dev_mode ${DEV_MODE}`);
+    })
+    process.on('SIGINT', () => {
+        console.log("SIGINT signal received")
+        SERVER.close(() => {
+            //DB.close_db();
+            process.exit(0);
+        })
+    })
+    process.on('SIGTERM', () => {
+        console.log("SIGTERM signal received")
+        SERVER.close(() => {
+            //DB.close_db();
+            process.exit(0);
+        })
+    })
+} else {
+    const key = fs.readFileSync(SSL_KEY_FILE);
+    const cert = fs.readFileSync(SSL_CERT_FILE);
+    const SERVER: Server = https.createServer({ key, cert }, APP);
+    process.on('SIGINT', () => {
+        console.log("SIGINT signal received")
+        SERVER.close(() => {
+            //DB.close_db();
+            process.exit(0);
+        })
+    })
+    process.on('SIGTERM', () => {
+        console.log("SIGTERM signal received")
+        SERVER.close(() => {
+            //DB.close_db();
+            process.exit(0);
+        })
+    })
+}
+
 // SERVER.listen(PORT, () => {
 //     console.log(`Server is listening on https://localhost:${PORT}`);
 //   });
-process.on('SIGINT',()=>{
-    console.log("SIGINT signal received")
-    SERVER.close(()=>{
-        //DB.close_db();
-        process.exit(0);
-    })
-})
-process.on('SIGTERM',()=>{
-    console.log("SIGTERM signal received")
-    SERVER.close(()=>{
-        //DB.close_db();
-        process.exit(0);
-    })
-})
