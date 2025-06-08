fd5308d0cf894a3514e689e42204d8aa8fc2608f
pradeep <pradeepbarik87@gmail.com>
2025-06-08 21:03:05 +0530

    dadsdsd

--- src/controller/cache-controller/index.ts
+++ src/controller/cache-controller/index.ts
@@ -1,8 +1,9 @@
 import fs from 'fs';
 import { Request, Response } from 'express';
 import Joi, { ValidationResult } from 'joi';
-import { parameterMissingResponse, internalServerError, successResponse } from '../../services/response';
+import { parameterMissingResponse, internalServerError, successResponse, unauthorizedResponse } from '../../services/response';
 import { cache_directory, clinic_management_cache } from '../../config';
+import { HOME_PAGE_CACHE_FILE, CARETAKER_SERVICE_HOMEPAGE_CACHE_FILE, CLINICS_PAGE_CACHE_FILE, DOCTORS_PAGE_CACHE_FILE, DOCTORS_DETAIL_CACHE_DIR, CLINICS_DETAIL_CACHE_DIR, MASSAGE_SERVICE_HOMEPAGE_CACHE_FILE, MASSAGE_SERVICE_CACHE_DIR } from '../../constants';
 const requestParams = {
     initClinicCacheDirectory: Joi.object({
         clinic_id: Joi.number().required(),
@@ -11,6 +12,138 @@ const requestParams = {
     })
 }
 const cacheController = {
+    clearCache: async (req: Request, res: Response) => {
+        const { tokenInfo } = res.locals;
+        if (typeof tokenInfo === 'undefined') {
+            unauthorizedResponse("permission denied! Please login to access", res);
+            return
+        }
+        if (req.query.cache_type === "all") {
+            try {
+                fs.accessSync(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}`);
+                fs.rmdir(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "all_category") {
+            try {
+                fs.accessSync(`${cache_directory}/category/all-categories.json`);
+                fs.unlink(`${cache_directory}/category/all-categories.json`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "category_doctor") {
+            try {
+                fs.accessSync(`${cache_directory}/category/DOCTOR.json`);
+                fs.unlink(`${cache_directory}/category/DOCTOR.json`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "category_clinic") {
+            try {
+                fs.accessSync(`${cache_directory}/category/CLINIC.json`);
+                fs.unlink(`${cache_directory}/category/CLINIC.json`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "category_disease") {
+            try {
+                fs.accessSync(`${cache_directory}/category/DISEASE.json`);
+                fs.unlink(`${cache_directory}/category/DISEASE.json`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "category_caretaker") {
+            try {
+                fs.accessSync(`${cache_directory}/category/CARETAKER.json`);
+                fs.unlink(`${cache_directory}/category/CARETAKER.json`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "service_available_cities") {
+            try {
+                fs.accessSync(`${cache_directory}/india/service-available-cities.json`);
+                fs.unlink(`${cache_directory}/category/service-available-cities.json`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "all_cities") {
+            try {
+                fs.accessSync(`${cache_directory}/india/all-cities.json`);
+                fs.unlink(`${cache_directory}/category/all-cities.json`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "home_page") {
+            try {
+                fs.accessSync(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${HOME_PAGE_CACHE_FILE}`);
+                fs.unlink(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${HOME_PAGE_CACHE_FILE}`, () => { })
+            } catch (err) {
+                internalServerError(`Something went wrong`, res);
+                return
+            }
+        } else if (req.query.cache_type === "doctors_home_page") {
+            try {
+                fs.accessSync(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${DOCTORS_PAGE_CACHE_FILE}`);
+                fs.unlink(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${DOCTORS_PAGE_CACHE_FILE}`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "all_doctor_details") {
+            try {
+                fs.accessSync(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${DOCTORS_DETAIL_CACHE_DIR}`);
+                await fs.promises.rm(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${DOCTORS_DETAIL_CACHE_DIR}`, { recursive: true, force: true });
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "clinics_home_page") {
+            try {
+                fs.accessSync(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${CLINICS_PAGE_CACHE_FILE}`);
+                fs.unlink(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${CLINICS_PAGE_CACHE_FILE}`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "all_clinic_details") {
+            try {
+                fs.accessSync(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${CLINICS_DETAIL_CACHE_DIR}`);
+                fs.promises.rm(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${CLINICS_DETAIL_CACHE_DIR}`, { recursive: true, force: true })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else if (req.query.cache_type === "caretakers_home_page") {
+            try {
+                fs.accessSync(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${CARETAKER_SERVICE_HOMEPAGE_CACHE_FILE}`);
+                fs.unlink(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${CARETAKER_SERVICE_HOMEPAGE_CACHE_FILE}`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res)
+                return
+            }
+        } else if (req.query.cache_type === "massage_service_home_page") {
+            try {
+                fs.accessSync(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${MASSAGE_SERVICE_HOMEPAGE_CACHE_FILE}`);
+                fs.unlink(`${cache_directory}/${tokenInfo.bs.toLowerCase()}/${tokenInfo.bd}/${MASSAGE_SERVICE_HOMEPAGE_CACHE_FILE}`, () => { })
+            } catch (err) {
+                internalServerError("Something went wrong", res);
+                return
+            }
+        } else {
+            parameterMissingResponse("Invalid cache type", res);
+            return
+        }
+        res.json(successResponse({}, "Cache cleared successfully"));
+    },
     initClinicCacheDirectory: async (req: Request, res: Response) => {
         const { query }: { query: any } = req;
         const validation: ValidationResult = requestParams.initClinicCacheDirectory.validate(query);
@@ -28,6 +161,7 @@ const cacheController = {
         } catch (err: any) {
             internalServerError(err.message, res)
         }
+
     }
 }
 export default cacheController;
--- src/routes/cache.ts
+++ src/routes/cache.ts
@@ -2,5 +2,6 @@ import {Router} from 'express';
 import {apiRateLimit,handelError} from '../middleware';
 import cacheController from '../controller/cache-controller';
 const cacheRoutes=Router();
+cacheRoutes.get("/clear-cache",[apiRateLimit(1,10)],cacheController.clearCache);
 cacheRoutes.get("/init-clinic-cache-directory",[apiRateLimit(1,10)],cacheController.initClinicCacheDirectory);
 export default cacheRoutes;
